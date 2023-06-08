60aa2d062f01d51b074246b3cb9558650650a7ba
root <root@DESKTOP-KKU56CK>
2023-06-08 19:56:53 -0400

    update please

--- style.cs
+++ style.cs
@@ -2,4 +2,21 @@
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+};
+
+div #new-player-form {
+
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+};
+div #all-players-container{
+     display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+
+};
+body{
+  background-color:RBG 6, 174, 213;
+
 }
