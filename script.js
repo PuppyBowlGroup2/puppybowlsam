162ea2d7c0646b2792406e53a0b8877ed96f014a
dhanajeane79 <117386832+dhanajeane79@users.noreply.github.com>
2023-06-10 22:59:31 -0400

    Updated CSS and added welcome message

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
 
+
+
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
 const cohortName = '2302-ACC-PT-WEB-PT-A';
 // Use the APIURL variable for fetch requests
