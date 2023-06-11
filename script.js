f52814652781f55bf3e1cb44e2375daa52909a97
dhanajeane79 <dhanajeane79@gmail.com>
2023-06-10 21:46:32 -0400

    Updated CSS for Player Cards

--- script.js
+++ script.js
@@ -73,9 +73,11 @@ const renderAllPlayers = async (players) => {
             <p>${player.breed}</p>
             <p>${player.status}</p>
             <img src=${player.imageUrl}/>
+            <div class="button-container">
             <button class="details-button" data-id="${player.id}">See Details</button>
-            <button class="Add-button" data-id="${player.id}">Add Player</button>
+           
             <button class="Remove-button" data-id="${player.id}">Remove From Roster</button>
+          </div>
             `;
             playerContainer.appendChild(playerElement);
 
@@ -105,18 +107,72 @@ const renderAllPlayers = async (players) => {
  */
 const renderNewPlayerForm = () => {
     try {
-        
-    } catch (error) {
-        console.error('Uh oh, trouble rendering the new player form!', error);
+        //createdAt and updatedAt should be created by the app, not the user. exclude them from the form
+        let form = `
+        <form>
+            <label>Name: </label><input type="text" name="name" placeholder="" required><br><br>
+            <label>ID: </label><input type="number" name="id" placeholder="" required><br><br>
+            <label>Breed: </label><input type="text" name="breed" placeholder="" required><br><br>
+            <label>Status: </label><select name="status">
+                <option value="bench">Bench</option>
+                <option value="field">Field</option>
+            </select><br><br>
+            <label>ImageUrl: </label><input type="url" name="imageUrl" placeholder="" required><br><br>
+            <label>TeamId: </label><input type="number" name="teamId" placeholder="" required><br><br>
+            <label>CohortId: </label><input type="number" name="cohortId" placeholder="" required><br><br>
+            <input type="submit" value="Add new Player">
+        </form>
+        `
+        newPlayerFormContainer.innerHTML = form;
+
+        //for submit events add the eventlistener to the entire form
+        newPlayerFormContainer.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            const name = document.getElementsByName('name')[0].value;
+            const id = document.getElementsByName('id')[0].value;
+            const breed = document.getElementsByName('breed')[0].value;
+            const status = document.getElementsByName('status')[0].value;
+            const imageUrl = document.getElementsByName('imageUrl')[0].value;
+
+            //createdAt and updateAt should be created by the app
+            const createdAt = new Date().getTime();
+
+            //updatedAt should be the same as createdAt when adding a new player. Change updatedAt if the player is edited and saved
+            const updatedAt = createdAt;
+
+            const teamId = document.getElementsByName('teamId')[0].value;
+            const cohortId = document.getElementsByName('cohortId')[0].value;
+
+            const newPlayer = {
+                id,
+                name,
+                breed,
+                status,
+                imageUrl,
+                createdAt,
+                updatedAt,
+                teamId,
+                cohortId
+            }
+
+            await addNewPlayer(newPlayer);
+            console.log(newPlayer);
+            await window.location.reload();
+        });
+    } catch (err) {
+        console.error('Uh oh, trouble rendering the new player form!', err);
     }
 }
 
 const init = async () => {
     const players = await fetchAllPlayers();
+    //console.log(players);
+    //console.log(typeof(players));
+    //console.log(Array.isArray(players));
     renderAllPlayers(players);
-   // renderNewPlayerForm();
+
+    renderNewPlayerForm();
 }
 
 init();
-
 
