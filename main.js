afeb824ebff671d6db5e0ba5fe16cce1e1641698
dhanajeane79 <117386832+dhanajeane79@users.noreply.github.com>
2023-06-14 20:56:30 -0400

    Cleaned up code

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-import { 
-  fetchAllPlayers,
-  fetchSinglePlayer,
-  renderPlayerDetails,
-  addNewPlayer,
-  removePlayer,
-  renderAllPlayers,
-  renderNewPlayerForm } from './players.js';
+// This code imports functions and modules from './players.js' and initializes the application by fetching all players, rendering them, and displaying a form for adding new players.
 
+// Importing necessary functions and modules from './players.js'
+import { 
+   fetchAllPlayers,
+   fetchSinglePlayer,
+   renderPlayerDetails,
+   addNewPlayer,
+   removePlayer,
+   renderAllPlayers,
+   renderNewPlayerForm } from './players.js';
+
+// Initializing the application
 const init = async () => {
+// Fetching all players asynchronously
 const players = await fetchAllPlayers();
+// Rendering all players
 renderAllPlayers(players);
+// Rendering the form for adding a new player
 renderNewPlayerForm();
 };
-
+// Calling the initialization function
 init();
